ded9f3c662fced34c9929810cb4a045b8c4c0aa1
Iulian <iuliancurcean2@gmail.com>
2022-11-01 23:19:22 +0200

    cu cateva erori

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,8 @@
+﻿namespace Curcean_Teodor_iulian_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}

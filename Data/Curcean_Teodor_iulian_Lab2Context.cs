a9db1e1b8222c1610cfe715a71b932c4fbcfa0f1
Iulian <iuliancurcean2@gmail.com>
2022-11-08 15:49:55 +0200

    lab5

--- Data/Curcean_Teodor_iulian_Lab2Context.cs
+++ Data/Curcean_Teodor_iulian_Lab2Context.cs
@@ -21,5 +21,9 @@ namespace Curcean_Teodor_iulian_Lab2.Data
         public DbSet<Curcean_Teodor_iulian_Lab2.Models.Author> Author { get; set; }
 
         public DbSet<Curcean_Teodor_iulian_Lab2.Models.Category> Category { get; set; }
+
+        public DbSet<Curcean_Teodor_iulian_Lab2.Models.Member> Member { get; set; }
+
+        public DbSet<Curcean_Teodor_iulian_Lab2.Models.Borrowing> Borrowing { get; set; }
     }
 }
--- Migrations/20221108133150_BorrowingsMembers.Designer.cs
+++ Migrations/20221108133150_BorrowingsMembers.Designer.cs
@@ -4,6 +4,7 @@ using Curcean_Teodor_iulian_Lab2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Curcean_Teodor_iulian_Lab2.Migrations
 {
     [DbContext(typeof(Curcean_Teodor_iulian_Lab2Context))]
-    partial class Curcean_Teodor_iulian_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20221108133150_BorrowingsMembers")]
+    partial class BorrowingsMembers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
